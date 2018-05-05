5b32f31e935bd328b9c4097baa36b821837d44f3
Ryan Irilli <ryanirilli@gmail.com>
2018-05-05 07:46:06 -0700

    add express server

---
+++ index.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use(express.static(path.join(__dirname, './client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
